247c2806e7a9c8b8511363a8f773af44b3f8ef85
0Sebbe <sebastian.stavenek@gmail.com>
2022-09-06 13:14:06 +0200

    Added start to menu and function to add students
    yers

--- Skola/src/Schoolsystem.cpp
+++ Skola/src/Schoolsystem.cpp
@@ -2,29 +2,74 @@
 
 void SchoolSystem::Run()
 {
-	int counter = 0;
-	while(true)
+	while (true)
 	{
 		//Menu
+		std::cout << "1. Add Student" << "\n";
+		std::cout << "2. Add Class" << "\n";
+		std::cout << "3. Add Student to Class" << "\n";
+		std::cout << "4. Search for Student" << "\n";
+		std::cout << "5. Search for Class" << "\n";
+
 		//Input
+		int input;
+		std::string nameInput;
+		int ageInput;
+		std::cin >> input;
+		switch (input)
+		{
+			case 1:
+				std::cout << "Add name: " << "\n";
+				std::cin >> nameInput;
+				std::cout << "Add age: " << "\n";
+				std::cin >> ageInput;
+				AddStudent(nameInput, ageInput);
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+		}
+
+		for (auto i : students) 
+		{
+			std::cout << i.name << " ";
+			std::cout << i.age << "\n \n";
+		}
 		//Feedback
-		std::cout << "Loop: " << counter << "\n";
-		counter++;
-		std::cin.get();
 
 	}
 }
 
-void SchoolSystem::AddStudent()
+void SchoolSystem::AddStudent(std::string newName, int newAge)
 {
 	Student student;
-	student.name = "Sebbe";
-	student.age = 17;
+	student.name = newName;
+	student.age = newAge;
 
 	students.push_back(student);
 }
 
 void SchoolSystem::RemoveStudent()
+{
+
+}
+
+void SchoolSystem::AddClass()
+{
+	
+}
+
+void SchoolSystem::RemoveClass()
 {
 
 }
--- Skola/src/Schoolsystem.h
+++ Skola/src/Schoolsystem.h
@@ -7,8 +7,10 @@ class SchoolSystem
 {
 public:
 	void Run();
-	void AddStudent();
+	void AddStudent(std::string newName, int newAge);
 	void RemoveStudent();
+	void AddClass();
+	void RemoveClass();
 
 	std::vector<Student> students;
 	std::vector<std::string> schoolclasses;
