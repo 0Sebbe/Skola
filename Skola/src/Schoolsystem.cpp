811ef307c7de3f3b96d668f8187b6a0634bb68c1
0Sebbe <sebastian.stavenek@gmail.com>
2022-09-13 13:03:00 +0200

    Problem med att printa elever från samma klass
    problemet är att när jag skriver vilken klass som ska printas ut, printas det fortfarande ingenting

--- Skola/src/Schoolsystem.cpp
+++ Skola/src/Schoolsystem.cpp
@@ -6,32 +6,69 @@ void SchoolSystem::Run()
 	{
 		//Menu
 		std::cout << "1. Add Student" << "\n";
-		std::cout << "2. Add Class" << "\n";
-		std::cout << "3. Add Student to Class" << "\n";
-		std::cout << "4. Search for Student" << "\n";
-		std::cout << "5. Search for Class" << "\n";
+		std::cout << "2. Search for Student" << "\n";
+		std::cout << "3. See all students in a class" << "\n";
+		std::cout << "4. ???" << "\n";
+		std::cout << "5. ???" << "\n";
 
 		//Input
 		int input;
 		std::string nameInput;
 		int ageInput;
+		std::string classInput;
+		std::string yesnoInput;
+
 		std::cin >> input;
 		switch (input)
 		{
-			case 1:
+		case 1:
+				//Adds a student to vector Student with name and age, optional to add c
 				std::cout << "Add name: " << "\n";
 				std::cin >> nameInput;
 				std::cout << "Add age: " << "\n";
 				std::cin >> ageInput;
-				AddStudent(nameInput, ageInput);
-
+				inputAdd:
+				std::cout << "Would you like to add a class to this student? YES or NO" << "\n \n";
+				std::cin >> yesnoInput;
+				if (yesnoInput == "YES") 
+				{
+					std::cout << "Add class: " << "\n \n";
+					std::cin >> classInput;
+					AddStudent(nameInput, ageInput, classInput);
+				}
+				else if (yesnoInput == "NO")
+				{
+					AddStudent(nameInput, ageInput, "");
+				}
+				else {
+					goto inputAdd;
+				}
+				
+				/*for (auto i : students)
+				{
+					std::cout << i.name << " ";
+					std::cout << i.age << " ";
+					std::cout << i.classes << "\n";
+				}*/
 				break;
+
+
 			case 2:
-
+				std::cout << "Type name to search for student: " << "\n";
+				std::cin >> nameInput;
+				FindStudent(nameInput);
 				break;
+
+
 			case 3:
-
+				//Prints a list with all students in a specific class
+				std::cout << "What class are you looking for?" << "\n\n";
+				std::cin >> nameInput;
+				
+				
 				break;
+
+
 			case 4:
 
 				break;
@@ -39,37 +76,56 @@ void SchoolSystem::Run()
 
 				break;
 		}
-
-		for (auto i : students) 
-		{
-			std::cout << i.name << " ";
-			std::cout << i.age << "\n \n";
-		}
+	
+		
 		//Feedback
 
 	}
 }
 
-void SchoolSystem::AddStudent(std::string newName, int newAge)
+void SchoolSystem::AddStudent(std::string newName, int newAge, std::string newClasses)
 {
 	Student student;
 	student.name = newName;
 	student.age = newAge;
+	student.classes = newClasses;
 
 	students.push_back(student);
 }
 
-void SchoolSystem::RemoveStudent()
+void SchoolSystem::FindStudent(std::string newName)
 {
+	Student student;
+	std::string name = "";
+	name = newName;
 
+	for (auto i : students) 
+	{
+		if (i.name == name)
+		{
+			std::cout << "Student found" << "\n";
+		}
+		else
+		{
+			std::cout << "Student not found" << "\n";
+		}
+	}
 }
 
-void SchoolSystem::AddClass()
+void SchoolSystem::SearchClass(std::string newClass)
 {
+	//Jag vill att man ska skriva in en klass och då ska det printas ut alla elever som har samma klass som jag har skrivit.
+	for (auto i : students)
+	{
+		if (i.classes == newClass)
+		{
+			std::cout << i.name << "\n";
+		}
+	}
 	
 }
 
 void SchoolSystem::RemoveClass()
 {
-
+	
 }
--- Skola/src/Schoolsystem.h
+++ Skola/src/Schoolsystem.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "Student.h"
+#include "Schoolclasses.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 class SchoolSystem
 {
 public:
 	void Run();
-	void AddStudent(std::string newName, int newAge);
-	void RemoveStudent();
-	void AddClass();
+	void AddStudent(std::string newName, int newAge, std::string newClasses);
+	void FindStudent(std::string newName);
+	void SearchClass(std::string newClass);
 	void RemoveClass();
 
 	std::vector<Student> students;
-	std::vector<std::string> schoolclasses;
+	std::vector<Schoolclasses> schoolclasses;
 };
